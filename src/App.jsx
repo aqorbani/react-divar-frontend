06c778fdb0c125a0f4ef008b56427e6b8b3bf68a
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-01 18:12:27 +0330

    create otp components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import AuthPage from "./pages/AuthPage";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">پروژه دیوار</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">پروژه دیوار</h1>
+      <AuthPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import SendOtpForm from "../components/templates/SendOtpForm";
+import CheckOtpForm from "../components/templates/CheckOtpForm";
+
+const AuthPage = () => {
+  const [step, setStep] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [code, setCode] = useState("");
+  return (
+    <div>
+      {step === 1 && <SendOtpForm />}
+      {step === 2 && <CheckOtpForm />}
+    </div>
+  );
+};
+
+export default AuthPage;
