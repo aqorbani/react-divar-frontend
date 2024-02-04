890b67d8bb5b5a18423d5ac002dc65eab059bc6f
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-04 17:17:30 +0330

    add sidebar style

--- src/components/templates/SideBar.jsx
+++ src/components/templates/SideBar.jsx
@@ -1,5 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCategory } from "src/services/admin";
+import Loader from "../modules/Loader";
+
 const SideBar = () => {
-  return <div>SideBar</div>;
+  const { data, isLoading } = useQuery({
+    queryKey: ["categories"],
+    queryFn: getCategory,
+  });
+  if (isLoading) return <Loader />;
+  return (
+    <div className="shadow-md p-4 m2">
+      <ul className="p-2">
+        {data?.data.map((item) => (
+          <li key={item._id} className="flex p-1 mt-2">
+            <img src={`${item.icon}.svg`} />
+            <p>{item.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default SideBar;
