ba1780b237590b21898638f14d469598134cfaa2
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-04 16:54:22 +0330

    add basic components for HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,13 @@
+import Main from "src/components/templates/Main";
+import SideBar from "src/components/templates/SideBar";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex">
+      <SideBar />
+      <Main />
+    </div>
+  );
 };
 
 export default HomePage;
