08ce903e72c392e5ac4bb6eb1e9b6bc18d299842
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-04 15:33:46 +0330

    show my post details in dashboard

--- src/components/templates/MyPostList.jsx
+++ src/components/templates/MyPostList.jsx
@@ -1,13 +1,31 @@
 import { useQuery } from "@tanstack/react-query";
 import { getMyPosts } from "src/services/user";
+import Loader from "../modules/Loader";
 
 const MyPostList = () => {
   const { data, isLoading } = useQuery({
     queryKey: ["myposts"],
     queryFn: getMyPosts,
   });
-  console.log({ data, isLoading });
-  return <div>MyPostList</div>;
+  if (isLoading) return <Loader />;
+  return (
+    <div>
+      <h3>آگهی های من</h3>
+      {data.data.posts.map((item) => (
+        <div key={item._id}>
+          <img src={`${import.meta.env.VITE_BASE_URL}${item.images[0]}`} />
+          <div>
+            <p>{item.options.title}</p>
+            <span>{item.options.content}</span>
+          </div>
+          <div>
+            <p>{item.createdAt}</p>
+            <span>{item.amount}تومان</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MyPostList;
