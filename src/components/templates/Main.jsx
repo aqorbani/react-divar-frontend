1f031c99e06509cc8f139e196cf2b8f1932d8829
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-08 00:52:17 +0330

    responsible card in products

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -11,25 +11,28 @@ const Main = () => {
   if (isLoading) return <Loader />;
   console.log(data);
   return (
-    <div className="flex flex-wrap w-full justify-between">
-      {data?.data.posts.map((item) => (
-        <div
-          key={item._id}
-          className="flex justify-between w-full lg:w-1/2 xl:w-1/3 p-2 shadow-md rounded"
-        >
-          <div>
-            <p>{item.options?.title}</p>
-            <div>
-              <p>{sp(item.amount)} تومان</p>
-              <span>{item.options?.city}</span>
+    <div className="flex flex-col w-full pr-4 pl-4">
+      <h3 className="m-5">آخرین آگهی ها</h3>
+      <hr className="mb-5" />
+      <div className="flex flex-wrap w-full justify-start">
+        {data?.data.posts.map((item) => (
+          <div key={item._id} className="flex w-full lg:w-1/2 xl:w-1/3 p-2">
+            <div className="flex justify-between w-full shadow-md rounded p-2">
+              <div>
+                <p>{item.options?.title}</p>
+                <div>
+                  <p>{sp(item.amount)} تومان</p>
+                  <span>{item.options?.city}</span>
+                </div>
+              </div>
+              <img
+                src={`${import.meta.env.VITE_BASE_URL}${item.images[0]}`}
+                className="w-32 h-32"
+              />
             </div>
           </div>
-          <img
-            src={`${import.meta.env.VITE_BASE_URL}${item.images[0]}`}
-            className="w-32 h-32"
-          />
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
