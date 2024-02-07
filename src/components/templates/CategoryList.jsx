2d72e4564a09206870416c1511c2f0674fc16879
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-07 12:04:08 +0330

    create remove option in category list

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -1,13 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCategory } from "src/services/admin";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { deleteCategory, getCategory } from "src/services/admin";
 import Loader from "../modules/Loader";
 
 const CategoryList = () => {
+  const queryClient = useQueryClient();
+
   const { data, isLoading, isFetching } = useQuery({
     queryKey: ["categories"],
     queryFn: getCategory,
   });
-  console.log({ data, isLoading });
+
+  const {
+    mutate,
+    data: newData,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: deleteCategory,
+    onSuccess: () => queryClient.invalidateQueries("categories"),
+  });
+  console.log({ newData, isPending, error });
+
+  if (isLoading) return <Loader />;
+
   return (
     <div>
       {isFetching ? (
@@ -18,6 +33,7 @@ const CategoryList = () => {
             <img src={`${item.icon}.svg`} alt="" />
             <h5>{item.name}</h5>
             <p>slug : {item.slug}</p>
+            <button onClick={() => mutate(item._id)}>حذف</button>
           </div>
         ))
       )}
